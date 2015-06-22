91bc8f9a9f75964e0dff0704d4f41f9e112329f4
patred <giuseppe.patred@gmail.com>
2015-06-22 14:21:47 +0200

    first servelet example

---
+++ src/main/java/it/synclab/srm/listener/ApplicationListener.java
@@ -0,0 +1,25 @@
+package it.synclab.srm.listener;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ApplicationListener extends HttpServlet {
+	
+	private static final long serialVersionUID = -4040473350732748067L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Hello Servlet Get</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+	
+}
