ec92a0cf5470da3d9881652136f329af997e5a34
patred <giuseppe.patred@gmail.com>
2015-06-22 12:46:51 +0200

    init

--- src/main/java/it/synclab/srm/persistent/entities/Candidate.java
+++ src/main/java/it/synclab/srm/persistent/entities/Candidate.java
@@ -1,5 +1,5 @@
 package it.synclab.srm.persistent.entities;
 
 public class Candidate {
-	
+	/* TODO */
 }
--- src/main/java/it/synclab/srm/persistent/service/CandidateService.java
+++ src/main/java/it/synclab/srm/persistent/service/CandidateService.java
@@ -3,5 +3,5 @@ package it.synclab.srm.persistent.service;
 import it.synclab.srm.persistent.entities.Candidate;
 
 public class CandidateService extends BasePersistentService<Candidate> {
-	
+	/* TODO */
 }
